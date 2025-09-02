71c7ed7ad0bc541d151ed9f66de3f4f290d79bd6
Satyamrk <satyamrk18@gmail.com>
2025-09-02 19:44:29 +0530

    git ignores added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
-
+import dotenv from "dotenv"
+dotenv.config()
 const app = express();
 //middle ware to print the post request body
 app.use(express.json());
@@ -201,6 +202,7 @@ app.put("/todos/:id", (req, res) => {
   }
 });
 
-app.listen(8080, () => {
-  console.log("server is running successfully ❤️‍🔥!!");
+const  PORT = process.env.PORT || 5003;
+app.listen(PORT, () => {
+  console.log(`server is running successfully ❤️‍🔥!! ${PORT}`);
 });
